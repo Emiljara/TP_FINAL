5a64ea3254404d35c44a0a3cf82540807318e07b
Emiljara <emilianocba1@gmail.com>
2024-10-18 17:06:26 -0300

    modified:   mi-ciudad/src/app/app.component.ts

--- mi-ciudad/src/app/app.component.ts
+++ mi-ciudad/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import {Platform} from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
-
+import { Network } from '@ionic-native/network/ngx';
+import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 
 export const environment = {
   loggedIn:false,
@@ -29,9 +31,47 @@ export class AppComponent {
       this.lastBack= Date.now();
   });
   }
-  
-  checkConnection(){
+}
+
+
+  // // checkConnection(){
+  //   export class checkConnection {
+  //     constructor(
+  //       private network: Network,
+  //       private http: HttpClient,
+  //       private toastController: ToastController
+  //     ) {}
+    
+  //     async verificarConexion() {
+  //       // Verificar la conexión de red
+  //       const tipoConexion = this.network.type;
+  //       if (!tipoConexion || tipoConexion === 'none') {
+  //         await this.mostrarToast('No hay conexión de red');
+  //         return;
+  //       }
+    
+  //       // Verificar la conexión con el backend
+  //       const urlBack = ''; // Cambia por la URL de tu backend
+  //       this.http.get(urlBack).subscribe(
+  //         async () => {
+  //           console.log('El backend está disponible');
+  //         },
+  //         async () => {
+  //           await this.mostrarToast('No se pudo conectar con el backend');
+  //         }
+  //       );
+  //     }
+    
+  //     // Función para mostrar el toast
+  //     async mostrarToast(mensaje: string) {
+  //       const toast = await this.toastController.create({
+  //         message: mensaje,
+  //         duration: 3000,
+  //         position: 'bottom',
+  //       });
+  //       await toast.present();
+  //     }
+  //   }
     // implementar chequea la coneccion de red y si tiene red chequea qla coneccion con el backend
     // si no tiene red o no tiene coneccion con el backend, muestra toast no tiene coneccion de red
-  }
-}
+
